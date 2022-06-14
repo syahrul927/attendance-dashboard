6ac8cc13e72bc40c967db5e1b7a28219e5f27220
Syahrul Ataufik <v-syahrul.ataufik@anteraja.id>
2022-06-15 00:43:33 +0700

    gatau

--- src/api/index.js
+++ src/api/index.js
@@ -1,19 +1,19 @@
-import axios from 'axios'
-const baseURL = 'https://attendance-serviceku.herokuapp.com'
+import axios from "axios"
+const baseURL = "https://attendance-serviceku.herokuapp.com"
 // const baseURL = 'http://localhost:3000'
 // const baseURL = '//159.223.56.146:3000'
 
-// const token = 
+// const token =
 
-const doRequest = async ({method, url, auth, contentType}, body = {}) => {
-    const token = await localStorage.getItem('token')
-    const headers = {'Authorization': auth && `Bearer ${token}`}
-    url = `${baseURL}${url}`
-    return await axios({
-        method,
-        url,
-        headers,
-        data: body
-    })
+const doRequest = async ({ method, url, auth, contentType }, body = {}) => {
+	const token = await localStorage.getItem("token")
+	const headers = { Authorization: auth && `Bearer ${token}` }
+	url = `${baseURL}${url}`
+	return await axios({
+		method,
+		url,
+		headers,
+		data: body,
+	})
 }
 export default doRequest
